36eed847399a2cb97e79354a5d3e8b8cdfe0ec13
Filippe Harnack <filippeharnack@gmail.com>
2026-03-13 14:39:03 -0300

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import ITInventoryDashboard from '@/components/ITInventoryDashboard'
+"use client";
+
+import dynamic from "next/dynamic";
+
+const ITInventoryDashboard = dynamic(
+  () => import("@/components/ITInventoryDashboard"),
+  { ssr: false }
+);
 
 export default function Home() {
-  return <ITInventoryDashboard />
+  return <ITInventoryDashboard />;
 }
